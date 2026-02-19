e33437857ceb816f37c8334ac2e681d393c645cc
PIPIIKING <luis.olivera.lo@gmail.com>
2026-02-18 23:06:16 -0300

    Project structure cleanup

---
+++ script.js
@@ -0,0 +1,16 @@
+// Scroll animation
+document.addEventListener("DOMContentLoaded", () => {
+
+  const sections = document.querySelectorAll(".fade-section");
+
+  const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+      }
+    });
+  }, { threshold: 0.2 });
+
+  sections.forEach(section => observer.observe(section));
+
+});
